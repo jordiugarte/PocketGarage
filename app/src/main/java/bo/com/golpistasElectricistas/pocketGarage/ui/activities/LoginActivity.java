99e4bde5b380c9a5faf8b87ace76fcb6703a0473
IgnacioDelRio <nachitotigredelriozenteno@gmail.com>
2020-10-10 16:27:42 -0400

    LoginActivity

---
+++ app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package bo.com.golpistasElectricistas.pocketGarage.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bo.com.golpistasElectricistas.pocketGarage.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

47fe9f6b5816583153ba2d853caa444099eaf400
jordiugarte <jordi933@gmail.com>
2020-10-14 09:47:58 -0400

    Register

--- app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/RegisterActivity.java
+++ app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/RegisterActivity.java
@@ -2,15 +2,23 @@ package bo.com.golpistasElectricistas.pocketGarage.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Window;
+
+import java.io.IOException;
 
 import bo.com.golpistasElectricistas.pocketGarage.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String LOG = LoginActivity.class.getName();
+
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,25 @@ public class RegisterActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+    }
+
+    public void choosePhoto() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select picture"), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

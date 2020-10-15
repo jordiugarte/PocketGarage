736c87b6a1984c570b71801702bfc8acbec35852
jordiugarte <jordi933@gmail.com>
2020-10-15 09:01:40 -0400

    New article ui

--- app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/NewArticleActivity.java
+++ app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/NewArticleActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import bo.com.golpistasElectricistas.pocketGarage.R;
+
 public class NewArticleActivity extends AppCompatActivity {
 
     @Override

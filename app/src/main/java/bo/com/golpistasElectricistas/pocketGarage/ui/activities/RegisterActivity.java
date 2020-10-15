ad8ed0e7f82ec15765b15ad85b5b68458028d9d8
jordiugarte <jordi933@gmail.com>
2020-10-14 23:44:31 -0400

    Register

--- app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/RegisterActivity.java
+++ app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/ui/activities/RegisterActivity.java
@@ -2,23 +2,33 @@ package bo.com.golpistasElectricistas.pocketGarage.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import bo.com.golpistasElectricistas.pocketGarage.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String LOG = LoginActivity.class.getName();
 
     private Bitmap bitmap;
+
+    private CircleImageView profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +37,14 @@ public class RegisterActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+        setViews();
     }
 
-    public void choosePhoto() {
+    private void setViews(){
+        profilePicture = findViewById(R.id.profileRegisterImage);
+    }
+
+    public void choosePhoto(View view) {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -43,9 +58,25 @@ public class RegisterActivity extends AppCompatActivity {
             Uri filePath = data.getData();
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                profilePicture.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            
+            //UploadPicture();
         }
+    }
+
+    private void UploadPicture(final String id, final String photo) {
+        final ProgressDialog progressDialog= new ProgressDialog(this);
+        progressDialog.setMessage("Uploading"); //TODO
+    }
+
+    public String getStringImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imageByteArray = byteArrayOutputStream.toByteArray();
+        String encodedImage = Base64.encodeToString(imageByteArray, Base64.DEFAULT);
+        return encodedImage;
     }
 }

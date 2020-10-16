62bad84b2fdd65776bb245434f7f5835f416a43d
Sergio Laguna <sergiol@arcediazadrianoutlook.onmicrosoft.com>
2020-10-15 20:08:21 -0400

    get y firebase para articulos

--- app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/utils/Constants.java
+++ app/src/main/java/bo/com/golpistasElectricistas/pocketGarage/utils/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final String INVALID_EMAIL_ERROR = "Ingrese un email válido";
     public static final String INCORRECT_EMAIL_ERROR = "El email es incorrecto";
     public static final String INCORRECT_PASSWORD_ERROR = "La contraseña es incorrecta";
+
+    //Api
+    public static final String BASE_URL="https://firebasestorage.googleapis.com/v0/b/pocket-garage-9ce4d.appspot.com/o/";
+    public static final String RESOURCE_ARTICLES = "json%2Farticles.json";
 }
